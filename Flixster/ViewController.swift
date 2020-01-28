af00804c6e7c5e8b9cdb8ee7a56fcdf48b66e2c3
Fabian Cooper <fcooper15@students.desu.edu>
2020-01-28 00:12:55 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Fabian Cooper on 1/28/20.
+//  Copyright © 2020 Fabian Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
