9a6440cd63c16779c12dd09634e2e788c0945f4a
Fabian Cooper <32171118+Tinodog@users.noreply.github.com>
2020-02-03 01:56:46 -0500

    Add files via upload

---
+++ Flix-Prep/MovieCell.swift
@@ -0,0 +1,24 @@
+ //
+//  MovieCell.swift
+//  Flix-Prep
+
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix-Prep/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix-Prep
+//
+//  Created by Fabian Cooper on 2/2/20.
+//  Copyright © 2020 Stephon Fonrose. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
